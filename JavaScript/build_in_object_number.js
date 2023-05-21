f59ac7e2446e0f168309c5e7843e5eb4e17f7722
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-05-21 21:41:06 +0900

    20230521

---
+++ JavaScript/build_in_object_number.js
@@ -0,0 +1,11 @@
+/*
+- Numberオブジェクトの定数
+`POSITIVE_INFINITY`：正の無限大 
+`NEGATIVE_INFINITY`：不の無限大
+→JavaScriptでは表現可能な数値の範囲を超えた場合の戻り値として利用される
+`NaN`：Not a Number → 数値として表現できない
+→NaNは自分自身を含む全ての数値と等しくない性質を持つ
+→NaNかどうか判断する際は`Number.isNaNメソッド`を使用する
+
+
+*/
