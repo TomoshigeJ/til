ebdb97deaa7a337517707cf30077dd00d862cb70
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-05-24 12:59:43 +0900

    20230524

---
+++ JavaScript/build_in_object_map.js
@@ -0,0 +1,70 @@
+/*
+マップの初期化
+new Map([[key, value], ...])
+	key:  キー
+	value:値
+*/
+let data = new Map([
+	['1st', 'ファースト'],
+	['2nd', 'セカンド'],
+	['3rd', 'サード'],
+]);
+console.log(data); // {'1st' => 'ファースト', '2nd' => 'セカンド', '3rd' => 'サード'}
+
+
+// マップの値を設定 / 取得
+let data = new Map();
+data.set(1, 'dog').set(2, 'rabbit').set(3, 'bear');
+console.log(data.get(2)); // rabbit
+
+
+// マップから既存キーを削除する
+let data = new Map();
+data.set(1, 'dog').set(2, 'rabbit').set(3, 'bear');
+console.log(data.delete(2)); // true
+console.log(data);           // {1 => 'dog', 3 => 'bear'}
+
+// 全消しする
+data.clear();
+console.log(data); // {size: 0}[[Entries]]No propertiessize: 0[[Prototype]]: Map
+
+
+/*
+マップから全てのキー/値を取得する
+keys：すべてのキーを取得
+values： すべての値を取得
+→それぞれの戻り値はイテレーターの為配列ではない。
+→配列を取得したい場合はfromメソッドを使用する
+*/
+let data = new Map();
+data.set(1, 'dog').set(2, 'rabbit').set(3, 'bear');
+
+for (let k of data.keys()) {
+	console.log(k);
+}
+
+for (let v of data.values()) {
+	console.log(v);
+}
+
+for (let [k, v] of data.entries()) {
+	console.log(`kの値は${k}、vの値は${v}`);
+}
+
+// 配列化する前とした後
+console.log(data.keys());             // MapIterator {1, 2, 3}
+console.log(Array.from(data.keys())); // [1, 2, 3]
+
+// マップの内容を順に処理するならforEach メソッドも利用できる
+let data = new Map();
+data.set(1, 'dog').set(2, 'rabbit').set(3, 'bear');
+
+data.forEach(function(v, k) {
+	console.log(`kの値は${k}、vの値は${v}`);
+});
+
+
+// マップのサイズを取得
+let data = new Map();
+data.set(1, 'dog').set(2, 'rabbit').set(3, 'bear');
+console.log(data.size); // 3
