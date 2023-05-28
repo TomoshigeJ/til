f9098440bd69827184b6ec2bd5f7e61a8512586f
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-05-28 17:11:08 +0900

    20230528

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -395,3 +395,31 @@ let list = [1, 2, 4, 8, 16];
 arrayWalk(list, (value, key) => console.log(`${key}: ${value}`));
 
 
+/* タグ付きテンプレート文字列
+テンプレート文字列：バックスラッシュ`` を利用して文字列に変数を埋め込むやつ */
+// 与えられた文字列をエスケープ処理
+function escapeHtml(str) {
+	if (!str) { return ''; }
+	str = str.replaceAll(/&/g, '&amp;');
+	str = str.replaceAll(/</g, '&lt;');
+	str = str.replaceAll(/>/g, '&gt;');
+	str = str.replaceAll(/"/g, '&quot;');
+	str = str.replaceAll(/'/g, '&#39;');
+	return str;
+}
+
+// 分解されたtemplatesとvaluesを順に連結(valuesはescapeHtml関数でエスケープ)
+function e(templates, ...values) {
+	let result = '';
+	for (let [i, temp] of templates.entries()) {
+		result += temp + escapeHtml(values[i]);
+	}
+	return result;
+}
+
+// テンプレート文字列をエスケープ処理
+let name = '<"Mario" & \'Luigi\'>';
+console.log(e`こんにちは、${name}さん!`);
+// => こんにちは、&lt;&quot;Mario&quot; &amp; &#39;Luigi&#39;&gt;さん!
+
+
