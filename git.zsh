1529fa63918e5bf7ebd4da0643032563ce016205
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-06-17 21:24:09 +0900

    $DATE

--- git.zsh
+++ git.zsh
@@ -1,9 +1,44 @@
 #!/bin/zsh
 ### お遊び
+### まとめて今日の日付でコミットしてプッシュ
+###########################################
+
+# 変数定義
+DATE=` date '+%Y%m%d' `
+
+# 関数定義
+function gitCommitAndPush {
+  git add .
+  git commit -m '$DATE'
+  git push origin main
+  echo コミットしてプッシュしました!
+}
 
 # 画面クリア
 clear
 
+# git status
+echo message: 以下のファイルをコミットしてプッシュしますか？
+echo message: コミットメッセージは $DATE です。
+echo message: git statusの結果
+echo -----------------------------------------------------------------
+git status
+echo -----------------------------------------------------------------
+
+# 入力待ち
+echo -n '実行しますか？ [y/n]: '
+read ANSWER
+
+case $ANSWER in
+  [y] )
+    echo 'Yes'
+    gitCommitAndPush
+    ;;
+  * )
+    echo '処理を中止しました。'
+    ;;
+esac
+
 # 指定URLを開く
-open https://github.com/TomoshigeJ
+#open https://github.com/TomoshigeJ
 
