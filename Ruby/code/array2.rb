6d2496d425f288ec682208950a7cef4a129b7202
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-07-04 21:05:51 +0900

    20230704

--- Ruby/code/array2.rb
+++ Ruby/code/array2.rb
@@ -1,5 +1,23 @@
 a = Array.new(5)
 p a
 p a.length
+p a.size
 
+# ブロックで初期値を指定の例
 p Array.new(2){|index| index + 10}
+
+# 第二引数で設定した初期値は同じオブジェクト
+a = Array.new(2, 'a')
+p a
+p a[0].equal? a[1]
+
+a[0].replace('b')
+p a
+
+# ブロックで指定すると同じオブジェクトにならない
+a = Array.new(2){'a'}
+p a
+p a[0].equal? a[1]
+
+a[0].replace('b')
+p a
