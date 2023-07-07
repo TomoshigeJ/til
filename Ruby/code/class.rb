e9ee419c983b22233fa68f845c8a1e138d247d2f
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-07-07 17:43:41 +0900

    20230707

--- Ruby/code/class.rb
+++ Ruby/code/class.rb
@@ -11,6 +11,8 @@ class Dog
   end
 end
 
+############################################
+
 # インスタンスの作成
 dog1 = Dog.new(1)
 dog2 = Dog.new(2)
@@ -20,3 +22,31 @@ p dog1.method1 # => 1
 p dog2.method1 # => 2
 dog1.method2   # => "わんわん"
 dog2.method2   # => "わんわん"
+
+# class式の評価順序
+p 1
+class T
+  p 2
+end
+p 3
+# => 実行すると1, 2, 3の順に出力される
+
+# classメソッド
+p dog1.class # => Dog
+
+############################################
+
+# クラスの継承
+class DogExt < Dog
+  def initialize(a, b)
+    @b = b
+    super a
+  end
+  def method3(c)
+    @a + @b + c
+  end
+end
+
+dogExt = DogExt.new(2, 8)
+p dogExt.method1    # => 2
+p dogExt.method3(3) # => 13
