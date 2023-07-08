9309f4e63af2753532c1a29ceeae794b3a84ad00
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-07-08 20:53:01 +0900

    20230708

--- Ruby/code/flatten.rb
+++ Ruby/code/flatten.rb
@@ -8,6 +8,16 @@ p array2.flatten  # => ["dog", "rabbit", "bear"]
 p array2.flatten! # => ["dog", "rabbit", "bear"]
 p array2          # => ["dog", "rabbit", "bear"]
 
-####
+p '-------------------------------'
+
+p ["apple", "banana", "orange"].flatten!
+# => nil
+
+p [["apple"],["banana"],["orange"]].flatten
+# => ["apple", "banana", "orange"]
+
 p ["apple", "banana", "orange"].reverse
+# => ["orange", "banana", "apple"]
+
 p %|apple banana orange|
+# => "apple banana orange"
