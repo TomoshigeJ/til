50dac194680379604724ecc22baaaa74b5980eb5
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-07-08 21:08:07 +0900

    20230708

--- Ruby/code/Enumerable.rb
+++ Ruby/code/Enumerable.rb
@@ -18,3 +18,19 @@ p [1, 2, 3].collect { |n| n + 1 }  # => [2, 3, 4]
 ]
 p arr.collect { |a| a ? 1 : 2 } # => [1, 2, 2, 1]
 p arr.collect { |a| a ? 1 : 2 }.inject(:+) # => 6
+
+# sample2
+a = [-1, 2, 3, 4, 5]
+b = (4..6).to_a
+
+p a.inject(:+) + b.inject(:+)
+# => 28
+
+p (a | b).inject(:-).abs + (a & b).inject(:+).abs
+# => 30
+
+p (a | b).inject(:*) + b.inject(0) { |x, y| x + y ** 3 }
+# => -315
+
+p ((a || b).map(&:succ).inject(:*) * (a && b).inject(:*).abs2 + 29)
+# => 29
