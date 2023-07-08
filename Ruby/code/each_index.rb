2f3e528cabcc0adc2c7e01740c033706d60452ef
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-07-08 16:27:26 +0900

    20230708

--- Ruby/code/each_index.rb
+++ Ruby/code/each_index.rb
@@ -6,3 +6,16 @@ p '----------------------------------'
 a.each_with_index { |n, i| puts n }
 p '----------------------------------'
 a.each_with_index { |n, i| puts i }
+p '----------------------------------'
+
+(10..15).to_a.map.with_index(1) do |elem, i|
+  puts i
+end
+
+(10..15).to_a.map.with_index(10) do |elem, i|
+  puts i
+end
+
+(10..15).to_a.map.with_index(100) do |elem, i|
+  puts i
+end
