95b104c7330d9bbbc8db0d9641514a4e88ee6c2e
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-07-08 17:22:54 +0900

    20230708

--- Ruby/code/Enumerable.rb
+++ Ruby/code/Enumerable.rb
@@ -8,3 +8,13 @@ p [1, 2, 3].inject(0){|result, item| result + item * 3 } # => 18
 # collectメソッド
 p [1, 2, 3].collect(&:to_s)        # => ["1", "2", "3"]
 p [1, 2, 3].collect { |n| n + 1 }  # => [2, 3, 4]
+
+ # sample
+ arr = [
+  true.equal?(true),
+  nil.eql?(NilClass),
+  String.new.equal?(String.new),
+  1.equal?(1)
+]
+p arr.collect { |a| a ? 1 : 2 } # => [1, 2, 2, 1]
+p arr.collect { |a| a ? 1 : 2 }.inject(:+) # => 6
