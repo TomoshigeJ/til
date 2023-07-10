3af0c84159fe6407999007989ee0cb2c72830409
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-07-10 16:12:56 +0900

    20230710

--- Ruby/code/split.rb
+++ Ruby/code/split.rb
@@ -1,2 +1,6 @@
 str = "1;2:3;4"
 p str.split(/;|:/)
+
+url = 'https://youtu.be/Jtk-ta8qW8o'
+p url.split('/')      # => ["https:", "", "youtu.be", "Jtk-ta8qW8o"]
+p url.split('/').last # => "Jtk-ta8qW8o"
